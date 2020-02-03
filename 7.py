d657732472ff4e273b2f8939b0a363c95ad53240
kontochrik <59823054+kontochrik@users.noreply.github.com>
2020-02-03 14:52:30 +0200

    Delete 7.py

--- 7.py
+++
@@ -1,9 +0,0 @@
-x1=input("Enter the first user:"))
-x2=input("Enter the second user:"))
-words1=0
-z=int(input("Enter for one's user")
-k=int(input("Enter for second's user")
-words1=0
-      for 1 in range (z-50,z):
-      tweets=int(input("enter tweets quantity")
-      words1=words1+tweets
